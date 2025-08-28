f37dd28d617b24438ca1bc4f45435a6c06e869b8
htilssu <tolashuu@gmail.com>
2025-08-29 06:47:59 +0700

    feat: rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import AppRoutes from './routes';
-import { NotificationProvider } from './controllers/NotificationController';
 import NotificationModal from './components/notification-modal';
+import { NotificationProvider } from "./controllers/useNotiProviderController";
+
 function App() {
-  return (
-    <NotificationProvider>
-      <AppRoutes />
-      <NotificationModal />
-    </NotificationProvider>
-  );
+    return (
+        <NotificationProvider>
+            <AppRoutes/>
+            <NotificationModal/>
+        </NotificationProvider>
+    );
 }
 
 export default App; 
